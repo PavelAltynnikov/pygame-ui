b12bb307fde2c468e2f1b416eb595d7b517797e0
PavelAltynnikov <robenzon11@gmail.com>
2022-07-31 21:16:46 +0400

    add test code on settings menu module

--- examples/settings_menu.py
+++ examples/settings_menu.py
@@ -4,3 +4,10 @@ from pygame_ui.form import Form
 class SettingsMenu(Form):
     def __init__(self, screen: pygame.surface.Surface, fps=60):
         super().__init__(screen, fps=fps)
+
+
+if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode((500, 500))
+    sm = SettingsMenu(screen)
+    sm.show()
